bfcafd2990df933064286a953cd2fdb4abef0f0e
Yonghee Kim <yonghee@yh.local>
2023-11-04 17:37:37 +0900

    Redirection once registered

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     name: String,
-    isProfileCeated: {
+    isProfileCreated: {
       type: Boolean,
       default: false,
     },
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,33 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/user')
+const User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   res.render('users/mypage', { title: 'my page' });
 });
-router.get('/new', function (req, res, next) {
-  res.render('users/new', { title: 'Register your info' });
+router.get('/new', function (req, res) {
+  if (req.user.isProfileCreated) {
+    res.redirect('/');
+  } else {
+    res.render('users/new', { title: 'Register your info' });
+  }
 });
 
-router.post('/', function (req, res, next) {
-})
+router.post('/', async function (req, res) {
+  await User.updateOne(
+    { googleId: req.user.googleId },
+    {
+      name: req.body.name,
+      interest: req.body.interest,
+      email: req.body.email,
+      isProfileCreated: true,
+    }
+  );
+  res.redirect('/');
+});
 
-router.get('/edit', function (req, res, next) {
+router.get('/edit', function (req, res) {
   res.render('users/edit', { title: 'Edit your info' });
 });
 
