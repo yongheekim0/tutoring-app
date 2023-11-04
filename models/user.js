ce0cffb4a190a786ce3bd3cbd9b0ea986afd680a
Yonghee Kim <yonghee@yh.local>
2023-11-04 15:47:40 +0900

    userSchema update

--- models/user.js
+++ models/user.js
@@ -4,6 +4,16 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     name: String,
+    isProfileCeated: {
+      type: Boolean,
+      default: false,
+    },
+    interest: {
+      type: String,
+      enum: ['Science', 'Language', 'Math', 'Self Develpment'],
+      lowercase: true,
+    },
+    photo: String,
     googleId: {
       type: String,
       required: true,
