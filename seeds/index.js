e0afb13a194827c3b7ee3303deb5e54b0dd1fb30
Yonghee Kim <yonghee@yh.local>
2023-11-06 15:59:11 +0900

    Seed data

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Lesson = require('../models/lesson');
 const User = require('../models/user');
+const { fakeUsers } = require('./users')
 
 const { users } = require('./tutors');
 
@@ -14,9 +15,12 @@ require('../config/passport');
 
 // };
 
-const seedDB = async () => {
-  await User.create({googleId: "234"})
-};
+const languages = ['English', 'Mandarin', 'French', 'Japanese', 'Korean', 'Spanish']
 
-seedDB();
+fakeUsers.forEach(async user => {
+  const data = new User(user)
+  await data.save()
+})
+ 
+
 // User.findOneAndUpdate({name: yonghee}, {isProfileCreated: false, interest: ''}).then((result)=> console.log(result))
