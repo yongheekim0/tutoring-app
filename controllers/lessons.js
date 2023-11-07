260829606682b3c9fe0554ae025d8ea01d66e1ab
Yonghee Kim <yonghee@yh.local>
2023-11-07 11:06:06 +0900

    Add bootstrap on my pages

--- controllers/lessons.js
+++ controllers/lessons.js
@@ -3,10 +3,12 @@ const Lesson = require('../models/lesson')
 const languages = ['English', 'Mandarin', 'French', 'Japanese', 'Korean', 'Spanish'];
 
 
-const index = (req, res) => {
+const index = async (req, res) => {
   if (!req.user) {
     res.redirect('/auth/google');
   }
+  // const lessons = await Lesson.find({'tutee.googleId': req.user.googldId})
+  // res.send(lessons)
   res.render('lessons/index')
 
 }
